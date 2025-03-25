c2896dcce0993120234d6f1ae6ae67ffd8d7ac90
michplunkett <5885605+michplunkett@users.noreply.github.com>
2025-03-24 21:58:08 -0500

    Create main.go

---
+++ cmd/deleteRedditHistory/main.go
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2025 Michael Plunkett (https://github.com/michplunkett)
+ * All rights reserved.
+ * Used to delete Reddit history of a given user.
+ */
+
+package main
+
+func main() {
+
+}
